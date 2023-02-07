72b6181a4d921b49360e8818fdae9d7f1677d010
1992aruna <113091758+1992aruna@users.noreply.github.com>
2023-02-06 23:06:36 -0800

    Add files via upload

---
+++ src/Barchart.js
@@ -0,0 +1,109 @@
+import {useEffect, useState} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+const options = {
+    indexAxis: 'x',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Bar Chart',
+      },
+    },
+  };
+
+const BarChart =() => {
+    const [data, setData] = useState({
+        labels:['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
+          {
+            label: 'No. of Projects',
+            data:[],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(25, 90, 132, 0.5)',
+          },
+          {
+            label: 'Active Projects',
+            data:[],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ],
+      });
+    useEffect(()=> {
+       const fetchData= async()=> {
+           const url = 'http://localhost:5000/task'
+           const labelSet = []
+           const dataSet1 = [];
+           const dataSet2 = [];
+         await fetch(url).then((data)=> {
+             console.log("Api data", data)
+             const res = data.json();
+             return res
+         }).then((res) => {
+             console.log("ressss", res)
+            for (const val of res) {
+                dataSet1.push(val.id);
+                dataSet2.push(val.postId)
+                labelSet.push(val.taskName)
+            }
+            setData({
+                labels:['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                datasets: [
+                  {
+                    label: 'Dataset ID',
+                    data:dataSet1,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(99, 132, 0.5)',
+                  },
+                  {
+                    label: 'Dataset ID2',
+                    data:dataSet2,
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 235, 0.5)',
+                  },
+                ],
+              })
+            console.log("arrData", dataSet1, dataSet2)
+         }).catch(e => {
+                console.log("error", e)
+            })
+        }
+        
+        fetchData();
+    },[])
+   
+    return(
+        <div style={{width:'80%', height:'50%'}}>
+            {
+                console.log("dataaaaaaaa", data)
+            }
+            <Bar data={data} options={options}/>
+         </div>)
+}
+export default BarChart;
